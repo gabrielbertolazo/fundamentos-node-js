3852cbfadd91054b9a4b11e855026ec628699f75
Gabriel Bertolazo <gabrielbertolazo@live.com>
2020-11-17 21:40:16 -0300

    finaliza o desafio com todos os tests ok

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value)
+      throw new Error('You do not have enough balance');
+
+    if (!title) throw new Error('Title is required');
+
+    if (!value) throw new Error('Value is required');
+
+    if (!type) throw new Error('Value is required');
+
+    if (!['income', 'outcome'].includes(type))
+      throw new Error('The type is invalid');
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
